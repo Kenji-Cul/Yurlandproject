dba631771e97cc0c3ed135edbeac5780a8b2b70f
Gideon Teibo <92151531+Kenji-Cul@users.noreply.github.com>
2022-11-01 16:13:46 +0100

    git commit -a

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+// Profile Slider
+let swiperVerse = new Swiper(".swiper-counter", {
+  loop: true,
+  spaceBetween: 24,
+  slidesPerView: "auto",
+  grabCursor: true,
+  autoplay: true,
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+    autoplay: true,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      spaceBetween: 48,
+    },
+  },
+});
+
 const selectedAll = document.querySelectorAll(".selected");
 
 selectedAll.forEach((selected) => {
